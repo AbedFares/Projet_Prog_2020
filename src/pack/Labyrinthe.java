b48406b516375b54ae55846b24abf676318c02d7
mohamedfares elabed <mohamedfares.elabed@ensi-uma.tn>
2020-04-19 00:44:42 +0200

    update

--- src/pack/Labyrinthe.java
+++ src/pack/Labyrinthe.java
@@ -72,76 +72,76 @@ public class Labyrinthe {
 	Scanner put = new Scanner(System.in);
 	System.out.println("entrer les positions de votre case de départ");//entrer les positions du case de départ
 	do{System.out.println("donner le n°ligne");
-	ligne=put.nextInt();}
+		ligne=put.nextInt();}
 	while(this.ligne_valide(ligne)==false);
 
 	do{System.out.println("donner le n°colone");
-	colone=put.nextInt();}
+		colone=put.nextInt();}
 	while(this.colone_valide(colone)==false);
 	laby[ligne][colone].set_caractere('D'); // caractere 'D' signifie case de Départ
 	laby[ligne][colone]=new Homme(ligne,colone);
 	System.out.println("entrer les positions de votre case d'arrivé");//entrer les positions du case d'arrivé
 	do{System.out.println("donner le n°ligne");
-	ligneA=put.nextInt();}
+		ligneA=put.nextInt();}
 	while(this.ligne_valide(ligneA)==false);
 
 	do{System.out.println("donner le n°colone");
-	coloneA=put.nextInt();}
+		coloneA=put.nextInt();}
 	while(this.colone_valide(coloneA)==false);	
 	laby[ligne][colone].set_caractere('X');// caractere 'X' signifie case d'arrivé
 
 	do{System.out.println("donner le nombre des virus de type covid19");//entrer le nb des virus covid
-	nbcovid=put.nextInt();}
+		nbcovid=put.nextInt();}
 	while((this.nombre_valide(nbcovid)==false)||(this.existeEspace(nbcovid)));                                      
 	System.out.println("vous devez positioner les virus covid19");             //positionnement des virus covid19                         
 	for(int i=0;i<nbcovid;i++) {         
 		do{System.out.println("donner le n°ligne de virus covid19"+(i+1));
-		nl=put.nextInt();}
+			nl=put.nextInt();}
 		while(this.ligne_valide(nl)==false);      
 		do{System.out.println("donner le n°colone de virus covid19"+(i+1));
-		nc=put.nextInt();}
+			nc=put.nextInt();}
 		while(this.colone_valide(nc)==false);
 		laby[nl][nc]=new Covid19(nl,nc);
 	}
 
-		do{System.out.println("donner le nombre des virus faible ");//entrer le nb des virus faible
+	do{System.out.println("donner le nombre des virus faible ");//entrer le nb des virus faible
 		nbVfaible=put.nextInt();}
-		while((this.nombre_valide(nbVfaible)==false)||(this.existeEspace(nbVfaible)));                                      
-		System.out.println("vous devez positioner les virus faible");                     //positionnement des virus faible                 
-		for(int i=0;i<nbVfaible;i++) {         
-			do{System.out.println("donner le n°ligne de virus faible"+(i+1));
+	while((this.nombre_valide(nbVfaible)==false)||(this.existeEspace(nbVfaible)));                                      
+	System.out.println("vous devez positioner les virus faible");                     //positionnement des virus faible                 
+	for(int i=0;i<nbVfaible;i++) {         
+		do{System.out.println("donner le n°ligne de virus faible"+(i+1));
 			nl1=put.nextInt();}
-			while(this.ligne_valide(nl1)==false);      
-			do{System.out.println("donner le n°colone de virus faible"+(i+1));
+		while(this.ligne_valide(nl1)==false);      
+		do{System.out.println("donner le n°colone de virus faible"+(i+1));
 			nc1=put.nextInt();}
-			while(this.colone_valide(nc1)==false);
-			laby[nl1][nc1]=new VirusFaible(nl1,nc1);}
+		while(this.colone_valide(nc1)==false);
+		laby[nl1][nc1]=new VirusFaible(nl1,nc1);}
 
-			do{System.out.println("donner le nombre du gel désinfectant ");//entrer le nb du gel désinfectant
-			nbgel=put.nextInt();}
-			while((this.nombre_valide(nbgel)==false)||(this.existeEspace(nbgel)));                                      
-			System.out.println("vous devez positioner le gel désinfectant");        //positionnement du gel désinfectant                
-			for(int i=0;i<nbgel;i++) {         
-				do{System.out.println("donner le n°ligne du gel désinfectant"+(i+1));
-				nl2=put.nextInt();}
-				while(this.ligne_valide(nl2)==false);      
-				do{System.out.println("donner le n°colone du gel désinfectant"+(i+1));
-				nc2=put.nextInt();}
-				while(this.colone_valide(nc2)==false);
-				laby[nl2][nc2]=new GelDesinfectant(nl2,nc2);}
+	do{System.out.println("donner le nombre du gel désinfectant ");//entrer le nb du gel désinfectant
+		nbgel=put.nextInt();}
+	while((this.nombre_valide(nbgel)==false)||(this.existeEspace(nbgel)));                                      
+	System.out.println("vous devez positioner le gel désinfectant");        //positionnement du gel désinfectant                
+	for(int i=0;i<nbgel;i++) {         
+		do{System.out.println("donner le n°ligne du gel désinfectant"+(i+1));
+			nl2=put.nextInt();}
+		while(this.ligne_valide(nl2)==false);      
+		do{System.out.println("donner le n°colone du gel désinfectant"+(i+1));
+			nc2=put.nextInt();}
+		while(this.colone_valide(nc2)==false);
+		laby[nl2][nc2]=new GelDesinfectant(nl2,nc2);}
 
-				do{System.out.println("donner le nombre des potions d'énergie ");//entrer le nb des potions d'énergie 
-				nbp=put.nextInt();}
-				while((this.nombre_valide(nbp)==false)||(this.existeEspace(nbp)));                                      
-				System.out.println("vous devez positioner les potions d'énergie");        //positionnement des potions d'énergie             
-				for(int i=0;i<nbp;i++) {         
-					do{System.out.println("donner le n°ligne du potion d'énergie"+(i+1));
-					nl3=put.nextInt();}
-					while(this.ligne_valide(nl3)==false);      
-					do{System.out.println("donner le n°colone du potion d'énergie"+(i+1));
-					nc3=put.nextInt();}
-					while(this.colone_valide(nc3)==false);
-					laby[nl3][nc3]=new PotionEnergie(nl3,nc3);}
+	do{System.out.println("donner le nombre des potions d'énergie ");//entrer le nb des potions d'énergie 
+		nbp=put.nextInt();}
+	while((this.nombre_valide(nbp)==false)||(this.existeEspace(nbp)));                                      
+	System.out.println("vous devez positioner les potions d'énergie");        //positionnement des potions d'énergie             
+	for(int i=0;i<nbp;i++) {         
+		do{System.out.println("donner le n°ligne du potion d'énergie"+(i+1));
+			nl3=put.nextInt();}
+		while(this.ligne_valide(nl3)==false);      
+		do{System.out.println("donner le n°colone du potion d'énergie"+(i+1));
+			nc3=put.nextInt();}
+		while(this.colone_valide(nc3)==false);
+			laby[nl3][nc3]=new PotionEnergie(nl3,nc3);}
 
 }
 }
